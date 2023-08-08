76cc4e393ff120914bd4fdeb15a0a43dedc6d615
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-08-08 15:26:40 -0400

    Adding philosophy

--- swallow-the-frog.py
+++ swallow-the-frog.py
@@ -1,11 +1,29 @@
 from PyInquirer import prompt
 import datetime
 import os
+from termcolor import colored
+
 
 '''
 The following script incorporates several important productivity concepts:
-...
+
+Swallowing the Frog: This is a concept popularized by Brian Tracy in his book "Eat That Frog!" The "frog" is the most challenging task of your day—the one you are most likely to procrastinate on, but also the one that can have the greatest positive impact on your life.
+
+Prioritizing: Prioritizing involves determining the order in which you will handle tasks based on their importance and urgency.
+
+Working on the Most Important Thing: This is the concept of focusing your efforts on the tasks that make the most significant difference.
+
+Experiential Avoidance: This is a concept from psychology that refers to a person's attempt to avoid thoughts, feelings, memories, physical sensations, and other internal experiences—even when doing so creates harm in the long-run.
+
+Accepting Discomfort: This is about understanding that discomfort is a natural part of life and work, and that avoiding it can actually hinder progress and growth.
+
+Not Getting Sidetracked by the Easy Task: This is about resisting the temptation to do easier, less important tasks instead of focusing on the more challenging, critical ones.
 '''
+
+
+# Clear the terminal screen
+os.system('clear')  # For Unix/Linux
+# os.system('cls')  # For Windows
 
 # Get the current date
 date = datetime.date.today()
@@ -17,7 +35,8 @@ dir_path = "/Users/jay/dropbox/roam/accountability/"
 file_path = f"{dir_path}tasks-{date}.org"
 
 # Step 1: Define Your Daily Goals and Tasks
-print("\nStep 1: Define Your Daily Goals and Tasks")
+
+print(colored("\nStep 1: Define Your Daily Goals and Tasks", 'white'))
 print("- Brainstorm a list of everything you need to do.")
 print("- Make sure each item is actionable and specific.")
 print("\nPlease enter your tasks for today, one per line. Press RETURN twice to finish.")
@@ -33,7 +52,7 @@ while True:
         break
 
 # Step 2: Identify the Most Important Task
-print("\nStep 2: Identify the Most Important Tasks")
+print(colored("\nStep 2: Identify the Most Important Tasks", 'white'))
 print("\nWhich tasks are the MOST IMPORTANT ONES? Use the arrow keys to move, SPACE to select, and RETURN to finish.")
 questions = [
     {
@@ -47,10 +66,11 @@ answers = prompt(questions)
 frogs = answers['frogs']
 
 # Step 3: Work on the Most Important Task First
-print("\nStep 3: Find the frog")
+print(colored("\nStep 3: Find the frog", 'white'))
 print("- Review your list of tasks and ask yourself: If I could only accomplish ONE task today, which one would have the greatest positive impact on my life or work?")
-print("- Label this task as your 'frog' for the day. This is the task you'll focus on first.")
-print("\nrank your most important tasks in order of priority. Hit RETURN to select.")
+print("- This task is your frog for the day --- the task you'll focus on first because it's the most important and probably also the most intimidating.")
+print("- By swallowing the frog first thing and staying on task until it's done, you'll strengthen your ability to accept discomfort.")
+print("\nRank your most important tasks in order of priority. Hit RETURN to select.")
 ranking = []
 for i in range(len(frogs)):
     questions = [
@@ -75,7 +95,7 @@ with open(file_path, "w") as f:
 
 # Display the file using bat
 print("- Start your workday by focusing on your 'frog'. Resist the temptation to start with easier, less important tasks.")
-print("- If the task is large or complex, break it down into smaller, manageable parts and start with the first part.")
+print("- If the task is large, complex, or otherwise daunting, break it down into smaller, manageable steps and start with the first step.")
 os.system(f'bat {file_path}')
 
 # Copy the output to the clipboard
